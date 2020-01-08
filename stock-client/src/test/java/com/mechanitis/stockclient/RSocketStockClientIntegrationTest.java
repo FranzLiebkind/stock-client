15bbcd054b8bc8c179248dbd11d85bfcb66228cc
FranzLiebkind <milomir.djuric@limit.rs>
2020-01-08 17:48:29 +0100

    Initial Commit: Basic structure and simple shape classes.Tests don't pass.

---
+++ stock-client/src/test/java/com/mechanitis/stockclient/RSocketStockClientIntegrationTest.java
@@ -0,0 +1,39 @@
+package com.mechanitis.stockclient;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.messaging.rsocket.RSocketRequester;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
+@SpringBootTest
+class RSocketStockClientIntegrationTest {
+
+    @Autowired
+    private RSocketRequester.Builder builder;
+
+    private RSocketRequester createRSocketRequester() {
+        return builder.connectTcp("localhost", 7000).block();
+    }
+
+    @Test
+    void shouldRetrieveStockPricesFromTheService() {
+        // given
+        RSocketStockClient rSocketStockClient = new RSocketStockClient(createRSocketRequester());
+
+        // when
+        Flux<StockPrice> prices = rSocketStockClient.pricesFor("SYMBOL");
+
+        // then
+        StepVerifier.create(prices.take(5))
+                .expectNextMatches(stockPrice -> stockPrice.getSymbol().equals("SYMBOL"))
+                .expectNextMatches(stockPrice -> stockPrice.getSymbol().equals("SYMBOL"))
+                .expectNextMatches(stockPrice -> stockPrice.getSymbol().equals("SYMBOL"))
+                .expectNextMatches(stockPrice -> stockPrice.getSymbol().equals("SYMBOL"))
+                .expectNextMatches(stockPrice -> stockPrice.getSymbol().equals("SYMBOL"))
+                .verifyComplete();
+    }
+}
